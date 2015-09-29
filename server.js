dd75c21d8b4144f04171178b80a2c47e303073d5
lentusignavus <ignavus.ii@gmail.com>
2015-09-28 17:58:38 -0700

    server.js file updated with mongo connection

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
 var port = process.env.PORT || 3000;
+//Basic require for mongodb
+require('./Models/AnswerModel.js');
+require('./Models/ConversationModel.js');
+require('./Models/QuestionModel.js');
+require('./Models/MessageModel.js');
+require('./Models/UserModel.js');
 
+//Adds error handling to mongoose.connect
+mongoose.connect("mongodb://localhost/FinalApp", function(err) {
+  console.log(arguments);
+  if (err) return console.log("Error connecting to database");
+  var x = new Date();
+  console.log("Connected to mongo at %s", x.toLocaleString());
+});
 
 app.set('views', path.join(__dirname, 'views'));
 //set the view engine that will render HTML from the server to the client
@@ -14,19 +28,21 @@ app.use(express.static(__dirname + '/bower_components'));
 //we want to render html files
 app.set('view engine', 'html');
 app.set('view options', {
-	layout: false
+  layout: false
 });
 
 //middleware that allows for us to parse JSON and UTF-8 from the body of an HTTP request
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 //on homepage load, render the index page
 app.get('/', function(req, res) {
-	res.render('index');
+  res.render('index');
 });
 
 var server = app.listen(port, function() {
-	var host = server.address().address;
-	console.log('Example app listening at http://localhost:' + port);
+  var host = server.address().address;
+  console.log('Example app listening at http://localhost:' + port);
 });
