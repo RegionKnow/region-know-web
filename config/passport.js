9ea88de25c53a08ab1bbfc7e52429ba98b5d6f3f
lentusignavus <ignavus.ii@gmail.com>
2015-10-02 10:46:45 -0700

    Added basic 404 page. Started the facebook and google authentication process. App registered with facebook

--- config/passport.js
+++ config/passport.js
@@ -5,6 +5,20 @@ var GoogleStrategy = require('passport-google').Strategy; //It's requiring the p
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+
+
+
 passport.use(new LocalStrategy(function(username, password, done) { //This password is called from password.authenticate, from user routes.
   User.findOne({username: username}) //find the username in the model from where it's being called.
   .exec(function(err, user) {
@@ -15,10 +29,26 @@ passport.use(new LocalStrategy(function(username, password, done) { //This passw
   });
 }));
 
-// passport.use(new FacebookStrategy(function () {
-//
-// })
-//
-// passport.use(new GoogleStrategy(function () {
-//
-// })
+passport.use(new FacebookStrategy({
+    clientID: 755751737864267,
+    clientSecret: '52a091ce024ba14be912204cf3dd17bc',
+    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    enableProof: false
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:3000/auth/google/return',
+    realm: 'http://localhost:3000/'
+  },
+  function(identifier, done) {
+    User.findByOpenID({ openId: identifier }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
