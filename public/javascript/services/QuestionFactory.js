06d3534924b5ca9c59ffddfd8bdf2c55c5c53249
Stuart_Grossman <grossman.stuart1@gmail.com>
2015-10-01 21:08:41 -0700

    more changes to question creation with tags

--- public/javascript/services/QuestionFactory.js
+++ public/javascript/services/QuestionFactory.js
@@ -6,18 +6,36 @@
 	function QuestionFactory($q, $http, $window, $rootScope) {
 		var o = {};
 
-		o.createQuestion = function(question){ // create the question
+		o.addTags = function(tags, question_id){
 			var q = $q.defer();
-			// console.log('this is the question in factory' + question)
-			$http.post('api/question/create', question).success(function(){
-				q.resolve();
+			$http.post('api/question/tags/' + question_id, tags).success(function(res){
+				q.resolve(res);
+			})
+			return q.promise;
+		}
+		o.getLocation = function(){
+			var key = 'AIzaSyDEQ3oCFj1hp7uqTeb8YLmXYrgtmQk-KmM'
+			var q = $q.defer();
+			console.log('in location homefactory')
+			$http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=' + key).success(function(res){
+				q.resolve(res);
+			
 			})
 			return q.promise;
 		}
 
-		o.findQuestions = function(){ // find all questions
+		o.createQuestion = function(question){ // create the question
 			var q = $q.defer();
-			$http.get('api/question').success(function(res){
+			// console.log('this is the question in factory' + question)
+			$http.post('api/question/create', question).success(function(res){
+				q.resolve(res);
+			})
+			return q.promise;
+		}
+
+		o.findQuestions = function(id){ // find all questions
+			var q = $q.defer();
+			$http.get('api/question/allquestions/' + id).success(function(res){
 				q.resolve(res);
 			})
 			return q.promise;
@@ -25,12 +43,14 @@
 
 		o.findQuestion = function(id){ // function to find indiv question
 			var q = $q.defer();
+			console.log(id)
 			$http.get('api/question/' + id).success(function(res){
-				// console.log('got past routes')
+				console.log('got past routes')
 				q.resolve(res);
 			})
 			return q.promise;
 		}
+		
 		o.addIdRef = function(Answer_id, id){
 			var q = $q.defer();
 			$http.post('api/question/' + id, Answer_id).success(function(res){
