22cea4043e3bcfa9f8b8e1ee1e9f9c57f9c187f2
Stuart_Grossman <grossman.stuart1@gmail.com>
2015-10-05 18:08:01 -0700

    alerts now sudo working, they delete after 30k ml, and display needs work

--- public/javascript/controllers/NavBarController.js
+++ public/javascript/controllers/NavBarController.js
@@ -25,6 +25,40 @@
 		$mdSidenav('left').close()
 	};
 
+	vm.alertObj = {}
+	alertWatch();
+	function alertWatch(){
+		console.log('watching for alerts')
+		$timeout(function() {
+			UserFactory.grabAlert(vm.status.id).then(function(res){
+				// console.log(res.alerts[0])
+				if(res.alerts.length > 0){
+					// console.log('grabbing alerts')
+					vm.alertObj.status = true;
+					vm.alertObj.alertNum = res.alerts.length;
+					vm.alertObj.alerts = res.alerts;
+					console.log(vm.alertObj)
+				}else{
+					// console.log('hiding alerts')
+					vm.alertStatus = false;
+				}
+
+			})
+			alertWatch();
+		}, 3000);
+		
+	}
+	vm.openAlerts = function(){
+		console.log('trying to open alerts')
+		vm.showAlertTab = true;
+		$timeout(function() {
+			UserFactory.deleteAlerts(vm.status.id).then(function(){
+				console.log('deleted alerts')
+				vm.alertObj.status = false;
+			})
+		}, 30000);
+		
+	}
 
 
 	//---------FUNCTIONALITY FOR REGISTER & LOGIN USER----------------------------------------------------------
