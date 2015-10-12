0b55ee4b8e2964e6c9aaae8166fc783f7f37ffc9
Stuart_Grossman <grossman.stuart1@gmail.com>
2015-10-12 14:15:51 -0700

    correct point grabing system. it is now online and working

--- public/javascript/controllers/RankController.js
+++ public/javascript/controllers/RankController.js
@@ -5,24 +5,24 @@
 
 	function RankController($state, $stateParams, RankFactory, UserFactory, QuestionFactory){
 
-		var vm = this;
-		var user_id = UserFactory.status._user.id
+		// var vm = this;
+		// var user_id = UserFactory.status._user.id
 
-		vm.generalPoints = function(){
-			RankFactory.getGeneralPoints(user_id).then(function(res){
-				// console.log(res)
-				vm.gP = res.count;
+		// vm.generalPoints = function(){
+		// 	RankFactory.getGeneralPoints(user_id).then(function(res){
+		// 		// console.log(res)
+		// 		vm.gP = res.count;
 
-			})
-		}
-		vm.knowledgePoints = function(){
-			QuestionFactory.getKpoints(user_id).then(function(res){
-				// console.log(res);
-				vm.kP = res.knowledgePoints;
-			})
-		}
-		vm.knowledgePoints();
-		vm.generalPoints();
+		// 	})
+		// }
+		// vm.knowledgePoints = function(){
+		// 	QuestionFactory.getKpoints(user_id).then(function(res){
+		// 		// console.log(res);
+		// 		vm.kP = res.knowledgePoints;
+		// 	})
+		// }
+		// vm.knowledgePoints();
+		// vm.generalPoints();
 
 	}
 
